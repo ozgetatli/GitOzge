a09226f6449b31b7974476536df3edd8190e5300
Syed Arslan  Ullah <114203538+Arslanullah@users.noreply.github.com>
2023-01-06 16:27:18 -0600

    Update Task1.java
    updated ozge..please check and let me know

--- src/Project_1/Task1.java
+++ src/Project_1/Task1.java
@@ -3,5 +3,8 @@ package Project_1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        // hello ozge i got your invite  and making some changes to your project
+        //Tamamaam
     }
 }
